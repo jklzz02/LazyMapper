877e828c7e492eae0bf047fb30269b4633f7dbe3
Jacopo <121625293+jklzz02@users.noreply.github.com>
2026-04-12 12:57:50 +0200

    Avoid to expose create map on map configuration. its only purpose is to configure the already defined map profile

--- LazyMapper.Lib/Configuration/IMapConfiguration.cs
+++ LazyMapper.Lib/Configuration/IMapConfiguration.cs
@@ -6,8 +6,5 @@ public interface IMapConfiguration<TSource, TDestination>
     where TSource : class, new()
     where TDestination : class, new()
 {
-    IMapConfiguration<TSource, TDestination> CreateMap(Action<MapProfile<TSource, TDestination>> mapConfigurations);
-    IMapConfiguration<TSource, TDestination> CreateMap();
-    
     void ReverseMap();
 }
