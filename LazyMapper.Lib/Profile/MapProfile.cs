077560be0383d83fd7d9f5f949e965d36c9cdebd
Jacopo <121625293+jklzz02@users.noreply.github.com>
2026-09-02 22:49:15 +0200

    No need to cut off records from being eligible for mapping

--- LazyMapper.Lib/Profile/MapProfile.cs
+++ LazyMapper.Lib/Profile/MapProfile.cs
@@ -11,8 +11,8 @@ namespace LazyMapper.Profile;
 /// <typeparam name="TSource">The source type. Must be a class with a parameterless constructor.</typeparam>
 /// <typeparam name="TDestination">The destination type. Must be a class with a parameterless constructor.</typeparam>
 public class MapProfile<TSource, TDestination> : IMapProfile
-    where TSource : class, new()
-    where TDestination : class, new()
+    where TSource :  new()
+    where TDestination : new()
 {
     private readonly Dictionary<BindingKey, MapBinding> _sourceBindings = new();
     private readonly Dictionary<BindingKey, MapBinding> _destinationBindings = new();
